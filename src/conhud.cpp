3a2ef4bba5c120e636dc5e302d5f3c21f7119085
Samuel Sarle <samuelsarle@gmail.com>
2018-02-06 21:40:22 +0200

    Added YOLO object detection.

--- src/conhud.cpp
+++ src/conhud.cpp
@@ -1,10 +1,5 @@
 #include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <SDL_opengl.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-//#include <GL/freeglut.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -12,19 +7,23 @@
 #include <cstdlib>
 #include <memory>
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 #include <osvr/ClientKit/ClientKit.h>
 #include <osvr/ClientKit/Display.h>
 
 #include "SDL2Helpers.h"
 
+#include "yolo_v2_class.hpp"
 
-cv::VideoCapture capture;
-cv::Mat frame;
 GLuint texture;
-void render(osvr::clientkit::DisplayConfig &display);
-void draw(cv::Mat img);
-static void initialization();
+void render(osvr::clientkit::DisplayConfig &display, cv::Mat frame);
+void drawToHMD(cv::Mat img);
+void drawBoxes(cv::Mat matIMG, std::vector<bbox_t> resultVec, std::vector<std::string> objectNames);
+void showConsoleResult(std::vector<bbox_t> const resultVec, std::vector<std::string> const objectNames);
+cv::Scalar objectIdToColor(int obj_id);
+std::vector<std::string> objectNamesFromFile(std::string const filename);
 static void handleEvents(bool* runningStatus);
 
 /********************
@@ -35,7 +34,15 @@ static void handleEvents(bool* runningStatus);
 
 int main(int argc, char* argv[]) {
 
-  initialization();
+  std::string namesFile = "darknet/data/voc.names";
+  std::string cfgFile = "darknet/cfg/tiny-yolo-voc.cfg";
+  std::string weightsFile = "darknet/tiny-yolo-voc.weights";
+//std::string cfgFile = "darknet/cfg/yolo-voc.cfg";
+//std::string weightsFile = "darknet/yolo-voc.weights";
+
+  Detector detector(cfgFile, weightsFile);
+
+  auto objectNames = objectNamesFromFile(namesFile);
 
 /*for brevity*/
   const auto CENTERED = SDL_WINDOWPOS_CENTERED;
@@ -73,6 +80,9 @@ int main(int argc, char* argv[]) {
       std::printf("Camera feed taken from external camera\n");
     }
   }
+
+  SDL_Init(SDL_INIT_VIDEO);
+
   SDL_Log("Current display mode is %dx%dpx", window_w, window_h);
 
   osvr::SDL2::Lib lib;
@@ -104,15 +114,17 @@ int main(int argc, char* argv[]) {
 
   glGenTextures(1, &texture);
 
-  cv::Mat frame;
-  capture >> frame;
-
+  cv::VideoCapture capture;
   capture.open(externalCam);
+  if (!capture.isOpened()) { return EXIT_FAILURE; }
 
-  if (!capture.isOpened()) {return EXIT_FAILURE;}
+  cv::Mat curFrame;
+  capture >> curFrame;
 
-  //capture.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-  //capture.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+  std::vector<bbox_t> resultVec;
+
+//capture.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+//capture.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 
 /**************
 *  main loop  *
@@ -124,27 +136,34 @@ int main(int argc, char* argv[]) {
 
     frameStart = SDL_GetTicks();
 
-    handleEvents(&isRunning);
-
 /*update clock*/
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     strftime(timeText, 80, "%H:%M:%S", timeinfo);
 
-/*update and render video feed*/
-    ctx.update();
-    render(display);
-    SDL_GL_SwapWindow(window.get());
+    handleEvents(&isRunning);
 
-/*slow down to 30FPS if running too fast*/
-    frameTime = SDL_GetTicks() - frameStart;
-    if (FRAMEDELAY > frameTime) {
-      SDL_Delay(FRAMEDELAY - frameTime);
+    capture >> curFrame;
+
+    if (!curFrame.empty()) {
+
+/*detect objects and draw a box around them*/
+      resultVec = detector.detect(curFrame);
+      drawBoxes(curFrame, resultVec, objectNames);
+//    showConsoleResult(resultVec, objectNames);    //uncomment this if you want console feedback
+
+/*update and render video feed*/
+      ctx.update();
+      cv::flip(curFrame, curFrame, 0);
+      render(display, curFrame);
+      SDL_GL_SwapWindow(window.get());
     }
+
+/*slow down to 30FPS if running faster*/
+    frameTime = SDL_GetTicks() - frameStart;
+    if (FRAMEDELAY > frameTime) { SDL_Delay(FRAMEDELAY - frameTime); }
   }
 
-  TTF_Quit();
-  IMG_Quit();
   SDL_Quit();
 
   cv::destroyAllWindows();
@@ -160,17 +179,6 @@ int main(int argc, char* argv[]) {
 *                   *
 ********************/
 
-void initialization() {
-  SDL_Init(SDL_INIT_VIDEO);
-  TTF_Init();
-
-  const int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
-  if (IMG_Init(imgFlags) != imgFlags) {
-    std::fprintf(stderr, "Error: %s", IMG_GetError());
-  }
-  std::printf("Initialization succesful\n");
-}
-
 /*keyboard input*/
 void handleEvents(bool* runningStatus) {
   SDL_Event ev;
@@ -178,7 +186,7 @@ void handleEvents(bool* runningStatus) {
     if (ev.type == SDL_QUIT) {
       *runningStatus = false;
     }
-    else if(ev.type == SDL_KEYDOWN) {
+    else if (ev.type == SDL_KEYDOWN) {
       switch(ev.key.keysym.sym) {
         case SDLK_ESCAPE:
           *runningStatus = false;
@@ -190,27 +198,21 @@ void handleEvents(bool* runningStatus) {
   }
 }
 
-void render(osvr::clientkit::DisplayConfig &display) {
-  display.forEachEye([](osvr::clientkit::Eye eye) {
-    eye.forEachSurface([](osvr::clientkit::Surface surface) {
+void render(osvr::clientkit::DisplayConfig &display, cv::Mat frame) {
+  display.forEachEye([&frame](osvr::clientkit::Eye eye) {
+    eye.forEachSurface([&frame](osvr::clientkit::Surface surface) {
       auto viewport = surface.getRelativeViewport();
       glViewport(static_cast<GLint>(viewport.left),
                  static_cast<GLint>(viewport.bottom),
                  static_cast<GLsizei>(viewport.width),
                  static_cast<GLsizei>(viewport.height));
-
       glLoadIdentity();
-
-      capture >> frame;
-
-      cv::flip(frame, frame, 0);
-
-      draw(frame);
+      drawToHMD(frame);
     });
   });
 }
 
-void draw(cv::Mat img) {
+void drawToHMD(cv::Mat img) {
   glBindTexture(GL_TEXTURE_2D, texture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -232,4 +234,47 @@ void draw(cv::Mat img) {
 
   glDisable(GL_TEXTURE_2D);
   glDeleteTextures(1, &texture);
+}
+
+void drawBoxes(cv::Mat matIMG, std::vector<bbox_t> resultVec, std::vector<std::string> objectNames) {
+  int const colors[6][3] = { {1,0,1},{0,0,1},{0,1,1},{0,1,0},{1,1,0},{1,0,0} };
+  for (auto &i : resultVec) {
+    cv::Scalar color = objectIdToColor(i.obj_id);
+    cv::rectangle(matIMG, cv::Rect(i.x, i.y, i.w, i.h), color, 5);
+    if (objectNames.size() > i.obj_id) {
+      std::string obj_name = objectNames[i.obj_id];
+      if (i.track_id > 0) { obj_name += " - " + std::to_string(i.track_id); }
+      cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
+      int const max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+      cv::rectangle(matIMG, cv::Point2f(std::max((int)i.x - 3, 0), std::max((int)i.y - 30, 0)), cv::Point2f(std::min((int)i.x + max_width, matIMG.cols - 1), std::min((int)i.y, matIMG.rows - 1)), color, CV_FILLED, 8, 0);
+      putText(matIMG, obj_name, cv::Point2f(i.x, i.y -10), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0,0,0), 2);
+    }
+  }
+}
+
+cv::Scalar objectIdToColor(int obj_id) {
+  int const colors[6][3] = { {1,0,1},{0,0,1},{0,1,1},{0,1,0},{1,1,0},{1,0,0} };
+  int const offset = obj_id * 123457 % 6;
+  int const color_scale = 150 + (obj_id * 123457) % 100;
+  cv::Scalar color(colors[offset][0],colors[offset][1],colors[offset][2]);
+  color *= color_scale;
+  return color;
+}
+
+void showConsoleResult(std::vector<bbox_t> const resultVec, std::vector<std::string> const objectNames) {
+  for (auto &i : resultVec) {
+    if (objectNames.size() > i.obj_id) { std::cout << objectNames[i.obj_id] << " - "; }
+    std::cout << "obj_id = " << i.obj_id << ",  x = " << i.x << ", y = " << i.y
+      << ", w = " << i.w << ", h = " << i.h
+      << std::setprecision(3) << ", prob = " << i.prob << std::endl;
+  }
+}
+
+std::vector<std::string> objectNamesFromFile(std::string const filename) {
+  std::ifstream file(filename);
+  std::vector<std::string> file_lines;
+  if (!file.is_open()) { return file_lines; }
+  for (std::string line; getline(file, line);) { file_lines.push_back(line); }
+  std::printf("Object names loaded\n");
+  return file_lines;
 }
